fbba85b3a80965fc6655fe5d22530236d1f54e6d
Siphosihle Sehume <sisehjhb025@student.wethinkcode.co.za>
2026-04-14 07:11:21 +0200

    Built my I/O pipelines as well as the `clientServer`.

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -15,10 +15,23 @@ public class Client {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             //Build `input` pipeline object
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            //Build `input through terminal`
+            //Build `input through the CMD line`
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
             ){
 
+            System.out.println("Server connected. Type JSSON and press enter: ");
+
+            //Sending data to server
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                //Send data to server
+                out.println(userInput);
+
+                //Wait for server responsive
+                String response = in.readLine();
+                System.out.println("Server responded " + response);
+
+                if ("quit".equalsIgnoreCase(userInput)) break;
             }
         }
     }
